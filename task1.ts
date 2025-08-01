ee82336b1118b58ee82a396af6b41a57953468b7
Gevorg Arzakanyan <zokavski@gmail.com>
2025-08-01 14:31:39 +0400

    initial commit

---
+++ task1.ts
@@ -0,0 +1,8 @@
+//  THE TASK
+
+// Create a TypeScript program that calculates the area of a rectangle using type annotations
+// Write a function that takes a variable number of arguments 
+// (using rest parameters) and returns their sum
+// Create an enum for days of the week and print the current day
+
+
---
+++ task2.ts
@@ -0,0 +1,31 @@
+// Task
+
+// 1. 
+    // Create an interface Person with properties like name, age, and email.
+    // Implement this interface in a class Employee and add a method to display employee info
+
+// 2. 
+    // Write a program that uses union types to handle different types of inputs (e.g., string or number)
+    // You will hava a handleInput function that will take both string and number input, do something based on input type
+
+// 3. 
+    // Create an abstract class Shape with an abstract method calculateArea()
+    // Extend it with classes like Circle and Rectangle
+    // Implement calculateArea in child classes
+
+
+// Testing the functionality
+// Test the Employee class
+// const emp = new Employee("John Doe", 30, "john.doe@example.com");
+// emp.displayInfo();
+
+// Test the union type function
+// handleInput("Hello World");
+// handleInput(42);
+
+// Test the shape classes
+// const circle = new Circle(5);
+// console.log(`Circle Area: ${circle.calculateArea()}`);
+
+// const rectangle = new Rectangle(10, 20);
+// console.log(`Rectangle Area: ${rectangle.calculateArea()}`);
